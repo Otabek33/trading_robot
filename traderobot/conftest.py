eb01ec5f24f51cfabc626e1bee734ed19e253cf6
Otabek33 <kebatotabek33@gmail.com>
2025-07-19 19:21:12 +0500

    first request

---
+++ traderobot/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from traderobot.users.models import User
+from traderobot.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
